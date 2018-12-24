0fde1ed462ecb2ef57590cb80324f5be63783df3
Isaac Adams <iadams@emoneyadvisor.com>
2018-12-24 13:24:01 -0500

    attempted to add a readable progress bar for bundles

--- tasks/utility.js
+++ tasks/utility.js
@@ -1,5 +1,6 @@
 let fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    progress = require('progress-stream');
 
 let e = module.exports;
 
@@ -20,9 +21,57 @@ e.createFile = (pathToFile) => {
     return fs.createWriteStream(pathToFile);
 };
 
+
+
 e.bundler = function (b, outputPath) {
     console.log('Building...');
-    return b.bundle()
+
+    let str = progress({
+        time: 100
+    }, async function (progress) {        
+        await process.stdout.write(Math.round(progress.percentage) + '%');
+    });
+
+    async function bundleLogger(err, buff) {
+        if(err)
+            console.log(err);
+
+        await process.stdout.write(buff);
+    }
+
+    //b.pipeline.on('transform', function (tr, file) {
+    //    console.log('transforming ' + path.basename(file));
+    //});
+
+    //b.pipeline.on('file', function (file, id, parent) {
+    //    console.log('bundling ' + path.basename(file));
+    //});
+
+    //b.pipeline.on('package', function (pkg) {
+    //    console.log('reading ' + pkg.name);
+    //});
+
+    //b.on('bundle', function (bundle) {
+    //    //console.log(bundle);
+    //    //return bundle.pipe(str);
+    //    bundle.on('data', log);
+    //    let writeStream = new fs.WriteStream(outputPath);
+
+    //    writeStream.on('error', function (err) {
+    //        console.log(err);
+    //    });
+
+    //    async function log(data) {
+    //        await console.log(data);
+    //        writeStream.write(data);
+    //    }
+    //});
+
+    
+    return b
+        .bundle()
+        //.pipe(str)
+        //.pipe(process.stdout)
         .pipe(e.createFile(outputPath));
 };
 
