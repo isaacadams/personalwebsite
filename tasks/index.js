2ecc0e51051aa712d79dd894e9b502f60f1d03a2
Isaac Adams <iadams@emoneyadvisor.com>
2019-02-03 17:48:51 -0500

    reorgnaized

--- tasks/index.js
+++ tasks/index.js
@@ -1,16 +1,4 @@
 //task that builds the index.html page
-let gulp = require('gulp'),
-    merge = require('merge-stream'),
-    browserify = require("browserify"),
-    babelify = require('babelify'),
-    rimraf = require('gulp-rimraf'),
-    lessify = require('lessify'),
-    cssify = require('cssify'),
-    b_minify = require('minifyify'),
-    b_uglify = require('uglifyify'),
-    fs = require('fs'),
-    path = require('path'),
-    progress = require('progress-stream');
 
 let { getJsonFile, ensureDirectoriesExist, createFile, bundler } = require('./tasks/utility');
 let settings = require('./tasks/settings');
