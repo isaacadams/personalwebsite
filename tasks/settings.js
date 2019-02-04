4d2bc8498dc443658aaa077e9bf7290c8022cb87
Isaac Adams <iadams@emoneyadvisor.com>
2019-02-03 21:20:16 -0500

    refactored settings

--- tasks/settings.js
+++ tasks/settings.js
@@ -1,21 +1,30 @@
 let e = module.exports;
 
-let source = 'src',
-    publish = 'dist',
-    publish_generated = publish + '/generated',
-    publish_js = publish_generated + '/js',
-    publish_styles = publish_generated + '/styles',
-    publish_data = publish_generated + '/data';
+class pathBuilder {
+    
+    constructor(source, publish) {
 
+        let publish_generated = `${publish}/generated`;
+
+        this.source = source;
+        this.publish = {
+            path: publish,
+            generated: publish_generated,
+            scripts: `${publish_generated}/js`,
+            styles: `${publish_generated}/styles`,
+            data: `${publish_generated}/data`,
+            images: `${publish}/assets/imgs`
+        };
+
+        String.prototype.rel = function () {
+            return this.replace(appPaths.publish.path, '');
+        };
+    }
+}
+
+let appPaths = new pathBuilder('src', 'dist');
 
 e.paths = {
-    source: source,
-    publish: {
-        path: publish,
-        generated: publish_generated,
-        scripts: publish_js,
-        styles: publish_styles,
-        data: publish_data,
-        images: publish + '/assets/imgs'
-    }
+    source: appPaths.source,
+    publish: appPaths.publish
 };
