cf80a5b1a679bd03e4e070e4645b0a20ed883412
Isaac Adams <iadams@emoneyadvisor.com>
2019-11-16 11:05:50 -0500

    more styling updates with breakpoint

--- src/Components/FailLog.tsx
+++ src/Components/FailLog.tsx
@@ -4,6 +4,7 @@ import { PageComponent } from './Shared/Page';
 interface ILog {
     date: Date;
     message: string;
+    tags: string[]; // a way to group failures into related categories
 }
 
 export class FailLog extends PageComponent<ILog> {
@@ -17,7 +18,17 @@ export class FailLog extends PageComponent<ILog> {
             return element;
         
         return (
-            <div className="container mt-4">
+            <div className="container my-4">
+                <div className="row pb-3">
+                    <h3 className="col-lg-auto col-12">
+                        What is this?
+                    </h3>
+                    <div className="col lead">
+                        Here is where you can find a log that I frequently update concerning tasks I failed to complete.
+                        I feel that this keeps me humble and motivated to keep pushing forward. 
+                        It also helps remind me what I have been working on or what I have left to do.
+                    </div>
+                </div>
                 {this.state.data.map((val, index) =>
                     <div key={index} className="row py-2">
                         <div className="col-2">{val.date}</div>
