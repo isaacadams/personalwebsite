674456413f277b04cad22106b725b286930302de
Isaac Adams <iadams@emoneyadvisor.com>
2019-12-06 20:02:39 -0500

    removing some extra space

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -6,7 +6,7 @@ import Navbar from './view/navbar';
 export const Main = () => (
     <Router>
         <div>
-            <section className="mt-5 py-5">&nbsp;</section>
+            <section className="py-5">&nbsp;</section>
             <div className="container mt-5">
                 <Navbar />
                 <Routes />
