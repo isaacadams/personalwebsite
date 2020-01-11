e20f5bd5c618df54b9860ff1c226d5af646e2af3
Isaac Adams <iadams@emoneyadvisor.com>
2020-01-11 15:35:34 -0500

    simplifying project page

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -25,27 +25,25 @@ export class Projects extends PageComponent<data> {
             return element;
        
         return (
-            <div className="mt-3">
-                <div className="row justify-content-center">
-                    <div className="col-auto mb-4">
-                        <ProjectCard 
-                            name="Game of War" 
-                            website="https://cards-gameofwar.web.app/"
-                            github="https://github.com/isaacadams/GameOfWar"
-                            image="assets/imgs/hide/cards.svg" 
-                            message="I created a simulation of the classic card game called 'Game of War'"
-                            tags={["react", "javascript", "css", "less"]} />
-                    </div>
-                    <div className="col-auto mb-4">
-                        <ProjectCard 
-                            name="Jenkins" 
-                            website="https://lightsail.isaacadams.me/jenkins/" 
-                            github="https://github.com/isaacadams/naruto"
-                            image="https://jenkins.io/images/logos/jenkins/jenkins.svg" 
-                            message="I host and maintain my own jenkins server which is used to build all of my public and private projects"
-                            tags={["docker", "linux", "lightsail", "nginx"]} />
-                    </div>
-                </div>                
+            <div className="mt-3 row justify-content-center">
+                <div className="col-auto mb-4">
+                    <ProjectCard 
+                        name="Game of War" 
+                        website="https://cards-gameofwar.web.app/"
+                        github="https://github.com/isaacadams/GameOfWar"
+                        image="assets/imgs/hide/cards.svg" 
+                        message="I created a simulation of the classic card game called 'Game of War'"
+                        tags={["react", "javascript", "css", "less"]} />
+                </div>
+                <div className="col-auto mb-4">
+                    <ProjectCard 
+                        name="Jenkins" 
+                        website="https://lightsail.isaacadams.me/jenkins/" 
+                        github="https://github.com/isaacadams/naruto"
+                        image="https://jenkins.io/images/logos/jenkins/jenkins.svg" 
+                        message="I host and maintain my own jenkins server which is used to build all of my public and private projects"
+                        tags={["docker", "linux", "lightsail", "nginx"]} />
+                </div>
             </div>
         );
     }
