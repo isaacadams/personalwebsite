1d95c7a91c19b5017f1a19f23265c4278fe59e30
Isaac Adams <iadams@emoneyadvisor.com>
2020-01-11 18:42:51 -0500

    padding only for large screens

--- src/routes/main.tsx
+++ src/routes/main.tsx
@@ -6,7 +6,7 @@ import Navbar from './view/navbar';
 export const Main = () => (
     <Router>
         <div className="container mt-lg-5">
-            <section className="row py-5">&nbsp;</section>
+            <section className="row py-lg-5">&nbsp;</section>
             <Routes />     
             <section className="row py-5">&nbsp;</section>      
         </div>            
