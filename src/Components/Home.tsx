af904ae307e87e3ad39a8bf7852054f777864a3e
Isaac Adams <iadams@emoneyadvisor.com>
2020-01-11 15:36:09 -0500

    removed commented out code

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -23,18 +23,16 @@ export class Home extends PageComponent<data> {
 
         return (
             <div className="container-lg px-lg-5">
-                    {this.state.data.map(d => 
-                        <div className="row pt-5 align-items-center justify-content-center">              
-                            <h3 className="col-12 col-lg-2 text-lg-right m-lg-0">
-                                {d.title}
-                            </h3>
-                            <span className="col-12 col-lg-6">
-                                {d.message}
-                            </span>
-                        </div>
-                    )}                    
-                {/* <Projects match={null} />
-                <Gallery match={null} /> */}                
+                {this.state.data.map(d =>
+                    <div className="row pt-5 align-items-center justify-content-center">
+                        <h3 className="col-12 col-lg-2 text-lg-right m-lg-0">
+                            {d.title}
+                        </h3>
+                        <span className="col-12 col-lg-6">
+                            {d.message}
+                        </span>
+                    </div>
+                )}
             </div>
         );
     }
