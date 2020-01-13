b176f7f20596939c455338b9a786c67d0bc0b150
Isaac Adams <iadams@emoneyadvisor.com>
2020-01-13 13:39:00 -0500

    making the input view component easier to read

--- src/Components/Shared/Inputs/Input.tsx
+++ src/Components/Shared/Inputs/Input.tsx
@@ -10,19 +10,29 @@ export abstract class Input extends React.Component<{
     value: any;
 }> {
     abstract FormatOptions;
+    
     constructor(props) {
         super(props);
         this.state = { value: this.props.initial };
     }
+    
     onValueChange(values) {
         let { value } = values;
         this.setState({ value: value });
         this.props.update(value);
     }
+
     render() {
         return <div className="row py-2">
-            <label className="col-12 col-lg-2 control-label">{this.props.name}</label>
-            <NumberFormat className="col-12 col-lg-10 form-control" name={this.props.name} value={this.state.value} {...this.FormatOptions} onValueChange={v => this.onValueChange(v)} />
+            <label className="col-12 col-lg-2 control-label">
+                {this.props.name}
+            </label>
+            <NumberFormat 
+                className="col-12 col-lg-10 form-control" 
+                name={this.props.name} 
+                value={this.state.value} 
+                {...this.FormatOptions} 
+                onValueChange={v => this.onValueChange(v)} />
         </div>;
     }
 }
