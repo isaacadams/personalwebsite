1f1f973980e2196c229c18127bfb024a84266f7a
Isaac Adams <iadams@emoneyadvisor.com>
2020-01-14 11:15:02 -0500

    adding in contributions

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -10,6 +10,7 @@ export class Calculator extends NavPageComponent {
         super(props);
         this.state = {
             principal: 100000,
+            contributions: 100,
             rate: 0.04,
             compound: 1,
             time: 30,
@@ -19,15 +20,20 @@ export class Calculator extends NavPageComponent {
         return (
             <div className="container">
                 <Currency initial={this.state.principal} name="principal" update={(v) => this.setState({ principal: v })} />
+                <Currency initial={this.state.contributions} name="contributions" update={(v) => this.setState({ contributions: v })} />
                 <Percent initial={this.state.rate} name="rate" update={(v) => this.setState({ rate: v })} />
                 <Number initial={this.state.compound} name="compound" update={(v) => this.setState({ compound: v })} />
                 <Number initial={this.state.time} name="time" update={(v) => this.setState({ time: v })} />
 
-                <CompoundInterestView 
+                <div className="row mt-4">
+                    <span className="col-2">Future Value</span>
+                    <CompoundInterestView className="col" 
                     p={this.state.principal} 
                     r={this.state.rate} 
                     n={this.state.compound} 
-                    t={this.state.time} />
+                    t={this.state.time}
+                    pmt={this.state.contributions} />
+                </div>                
             </div>
         );
     }
@@ -36,20 +42,27 @@ export class Calculator extends NavPageComponent {
 /**
  * @param p the initial principal amount
  * @param r the interest rate
- * @param n number of times interest rate compounds within one unit of 't'
+ * @param n the number of times interest rate compounds within one unit of 't'
  * @param t life of loan
+ * @param pmt the amount of money contributed per unit of t
  * @returns the accrued value of investment at the end
  */
-function CompoundInterest(p: number, r: number, n: number, t: number): number {
-    let pm = Math.pow(1 + r/n, n * t);
-    let final = p * pm;
-    return final;
+function CompoundInterest(p: number, r: number, n: number, t: number, pmt: number): number {
+    let multiplier = Math.pow(1 + r/n, n * t);
+    let a = p * multiplier;
+    let s = pmt * (multiplier - 1) / (r/n);
+    return a + s;
 }
 
 function CompoundInterestView(props) {
-    let final = CompoundInterest(props.p, props.r, props.n, props.t);
+    let final = CompoundInterest(props.p, props.r, props.n, props.t, props.pmt);
 
     return (
-        <NumberFormat value={final} displayType={'text'} thousandSeparator={true} prefix={'$'} decimalScale={2} />
+        <NumberFormat 
+            value={final} 
+            displayType={'text'} 
+            thousandSeparator={true} 
+            prefix={'$'} 
+            decimalScale={2} />
     );
 }
