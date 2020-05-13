8cc1305a7eac00bd862c2d119f02f5285763c360
Isaac Adams <isaac.d.adams@gmail.com>
2020-05-13 18:49:43 -0400

    removing hot reloading

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Main } from './routes/main';
 
-const render = () => {
-    console.log('hot reloading...');
-  ReactDOM.render(
-    <Main />,
-    document.getElementById('app')
-  );
-};
-
-render();
+ReactDOM.render(<Main />, document.getElementById('app'));
