612cf7797e784d16b226f7c5df9891f5994ceee4
Isaac Adams <isaac.d.adams@gmail.com>
2020-05-15 13:12:58 -0400

    better organization

--- src/routes/RouteDefinitions.ts
+++ src/routes/RouteDefinitions.ts
@@ -7,37 +7,40 @@ import { RouteModels } from './RouteModels';
 import { Landing } from '../Components/Landing';
 import { BibleStudy } from '../Components/BibleStudy';
 import { Calculator } from '../Components/Calculator';
-//import { ToolInfo } from "../Components/ToolInfo";
 import {ResumePage} from '../Components/Resume';
+import SignInPage from '../firebase/SignInPage';
+//import { ToolInfo } from "../Components/ToolInfo";
 
 class RouteConfiguration {
-    /* Home: RouteModels.Definition;
-    FailLog: RouteModels.Definition;
-    Projects: RouteModels.Definition;
-    Gallery: RouteModels.Definition; */
     Landing: RouteModels.Definition;
     BibleStudy: RouteModels.Definition;
     Calculator: RouteModels.Definition;
     Resume: RouteModels.Definition;
+    /* Home: RouteModels.Definition;
+    FailLog: RouteModels.Definition;
+    Projects: RouteModels.Definition;
+    Gallery: RouteModels.Definition; */
     //ToolInfo: RouteModels.Definition;
     private _configArray: RouteModels.Definition[];
+    SignIn: RouteModels.Definition;
 
     constructor() {
-        /* this.Home = RouteBuilder.define("Home", "/", Home, o => {
-            o.exact = true;
-        });
-        this.Projects = RouteBuilder.define("Projects", "/projects", Projects);
-        this.Gallery = RouteBuilder.define("Gallery", "/gallery", Gallery); */
-        /* this.FailLog = RouteBuilder.define("fail.log", "/fails", FailLog); */
 
-        this.Landing = RouteBuilder.define("Landing", "/", Landing,
-        o => {
+        this.Landing = RouteBuilder.define("home", "/", Landing, o => {
             o.exact = true;
         });
 
         this.BibleStudy = RouteBuilder.define("biblestudy", "/study", BibleStudy);
         this.Calculator = RouteBuilder.define("calculator", "/calculator", Calculator);
         this.Resume = RouteBuilder.define("resume", "/resume", ResumePage);
+        this.SignIn = RouteBuilder.define("signin", "/signin", SignInPage);
+
+        /* this.Home = RouteBuilder.define("Home", "/", Home, o => {
+            o.exact = true;
+        });
+        this.Projects = RouteBuilder.define("Projects", "/projects", Projects);
+        this.Gallery = RouteBuilder.define("Gallery", "/gallery", Gallery); */
+        /* this.FailLog = RouteBuilder.define("fail.log", "/fails", FailLog); */
         //this.ToolInfo = RouteBuilder.define("toolinfo", "/toolinfo", ToolInfo);
 
         this._configArray = Object.keys(this).map(k => this[k]);
