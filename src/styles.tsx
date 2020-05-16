92a9d689f24288d8ac9fe0a98ed71f3d0fbf15ac
Isaac Adams <isaac.d.adams@gmail.com>
2020-05-16 16:59:04 -0400

    adding plus sign to icons

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,8 +1,8 @@
 //import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faCheck, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { faCheck, faSpinner, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faGithub, faGoogle } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faCheck, faSpinner, faGithub, faGoogle);
+library.add(faCheck, faPlus, faSpinner, faGithub, faGoogle);
 dom.watch();
