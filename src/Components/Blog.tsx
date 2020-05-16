daf6b046a8a82173b45cbf4f60ed34f202b7888c
Isaac Adams <isaac.d.adams@gmail.com>
2020-05-16 13:40:20 -0400

    separating blog posts view

--- src/Components/Blog.tsx
+++ src/Components/Blog.tsx
@@ -3,16 +3,12 @@ import {GetSignInMethods, writeNewPost, readUserPosts} from '../firebase/databas
 import createWithAuth from '../firebase/createWithAuth';
 import { WrappedComponentProps } from 'react-with-firebase-auth';
 
-function Blog({
-    user,
-    error,
-    loading,
-  }: WrappedComponentProps) {
+function Blog({user, error, loading }: WrappedComponentProps) {
     let [content, setContent] = React.useState("");
     let [posts, setPosts] = React.useState([]);
-    
+
     //console.log("Hello Worlds");
-    
+
     React.useEffect(() => {
         if(user) readUserPosts(user.uid).then(r => setPosts(r));
     }, [user]);
@@ -26,12 +22,20 @@ function Blog({
             <div className="col-6">
                 <button className="btn btn-outline-dark" onClick={() => readUserPosts(user.uid).then(r => setPosts(r))}>Read</button>
                 {posts.map((p, i) => <div key={i}>
-                    <h3>{p.title}</h3>
-                    <p>{p.body}</p>
+                    <BlogPostView {...p} />
                 </div>)}
             </div>
         </div>
     );
-  }
+}
+
+function BlogPostView({title, body}) {
+    return (
+        <React.Fragment>
+            <h3>{title}</h3>
+            <p>{body}</p>
+        </React.Fragment>
+    );
+}
 
 export default createWithAuth(Blog);
