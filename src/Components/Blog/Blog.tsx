af158095277e5924bd8d47c68ff68f7f6c5bde11
Isaac Adams <isaac.d.adams@gmail.com>
2020-11-28 16:24:23 -0500

    cleanup

--- src/Components/Blog/Blog.tsx
+++ src/Components/Blog/Blog.tsx
@@ -1,19 +1,18 @@
 import * as React from 'react';
 import createWithAuth from '../../firebase/createWithAuth';
 import {WrappedComponentProps} from 'react-with-firebase-auth';
-import {ShowLoading} from '../Shared/ShowLoading';
 import {Route, Switch, useRouteMatch} from 'react-router-dom';
 import {AddBlogPost} from './AddBlogPost';
 import {BlogPostView} from './BlogPostView';
 import {BlogFeed} from './BlogFeed';
 import {useBlogPosts} from '../../firebase/useBlogPosts';
-import {Box} from 'grommet';
+import Loader from '../Shared/Loader';
 
 // text editors
 // https://reactjsexample.com/tag/editor/
 
 function Blog({user, error, loading}: WrappedComponentProps) {
-  if (loading) return <ShowLoading />;
+  if (loading) return <Loader />;
 
   let {data, ...blogProps} = useBlogPosts();
   let {path, url} = useRouteMatch();
