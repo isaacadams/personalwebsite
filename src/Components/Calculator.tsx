c56a1f238c97428fe4cb16a475ed46791beee052
Isaac Adams <isaac.d.adams@gmail.com>
2020-11-29 15:10:16 -0500

    cleaning up

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,20 +1,12 @@
-import {NavPageComponent} from './Shared/NavPageComponent';
 import React from 'react';
 import {CompoundInterestCalculator} from './Calculators/CompoundInterest';
 import {LoanPaymentCalculator} from './Calculators/Loan';
 
-export class Calculator extends NavPageComponent {
-  constructor(props: any) {
-    super(props);
-  }
-  render() {
-    return (
-      <div>
-        <div>
-          <CompoundInterestCalculator />
-          <LoanPaymentCalculator />
-        </div>
-      </div>
-    );
-  }
+export function Calculator(props) {
+  return (
+    <>
+      <CompoundInterestCalculator />
+      <LoanPaymentCalculator />
+    </>
+  );
 }
