e5c3c5e0fd72ca7baaca148a11bb6846c596af95
Isaac Adams <isaac.d.adams@gmail.com>
2021-08-23 08:22:18 -0400

    updating resume

--- src/Components/Resume.tsx
+++ src/Components/Resume.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import {Resume} from '@isaacadams/resume';
 import {Box} from 'grommet';
+import styled from 'styled-components';
+
+export const ResumeWrapper = styled.div`
+  #my-resume .content {
+    width: 660pt;
+  }
+`;
 
 export function ResumePage(props) {
   return (
     <Box fill="horizontal" responsive>
-      <Resume />
+      <ResumeWrapper>
+        <Resume />
+      </ResumeWrapper>
     </Box>
   );
 }
